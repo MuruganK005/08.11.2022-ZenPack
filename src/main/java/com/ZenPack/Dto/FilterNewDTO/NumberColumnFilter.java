ec66bf1221e20700ba0fb32a2544724c32c2ed34
vtguser <muruganeee005@gmai.com>
2022-11-09 12:52:35 +0530

    09.11.2022

---
+++ src/main/java/com/ZenPack/Dto/FilterNewDTO/NumberColumnFilter.java
@@ -0,0 +1,18 @@
+package com.ZenPack.Dto.FilterNewDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class NumberColumnFilter extends ColumnFilter{
+    private String type;
+    private Integer filter;
+    private Integer filterTo;
+
+    public String getFilterType() {
+        return filterType;
+    }
+}
